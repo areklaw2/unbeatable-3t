977e3c18c9586b55d6d5a73a6c09e797a89d0a9f
Aza Walker <azawalker.jr@gmail.com>
2026-07-01 00:21:06 -0400

    chore: adding tailwind

--- packages/web/src/main.rs
+++ packages/web/src/main.rs
@@ -18,6 +18,7 @@ enum Route {
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
+const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
     dioxus::launch(App);
@@ -31,6 +32,7 @@ fn App() -> Element {
         // Global app resources
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Stylesheet { href: TAILWIND_CSS }
 
         Router::<Route> {}
     }
