221888766a136a966f199904814af465956bf84e
Aza Walker <azawalker.jr@gmail.com>
2026-07-05 21:55:51 -0400

    chore: delete favicon

--- packages/web/src/main.rs
+++ packages/web/src/main.rs
@@ -22,7 +22,6 @@ enum Route {
     Game { room_id: Option<String> },
 }
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
 const THEME_CSS: Asset = asset!("/assets/dx-components-theme.css");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 const FONT_CSS: &str =
@@ -36,7 +35,6 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-        document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: FONT_CSS }
         document::Stylesheet { href: THEME_CSS }
         document::Stylesheet { href: MAIN_CSS }
